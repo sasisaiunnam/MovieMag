076f1777422569ecc88dbeb14dd8294aaa6fcdaf
sasi unnam <sasisai.unnam@gmail.com>
2026-04-09 14:12:50 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Header from './Components/Header/Header'
 import Home from './Components/Home/Home';
 import { Routes,Route } from 'react-router-dom';
 import MovieDetails from './Components/MovieDetails/MovieDetails';
 import Genre from './Components/genre/genre';
-
+import Header from "./Components/Header/Header"
 
 function App() {
   return (
